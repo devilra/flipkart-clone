a41a130ab34d67b9797fb6ce44971d2a0c589a03
devilra <rockerraja906@gmail.com>
2025-09-19 15:29:27 +0530

    initialize

--- frontent/app/api.js
+++ frontent/app/api.js
@@ -1,7 +1,8 @@
 const { default: axios } = require("axios");
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  //baseURL: "http://localhost:4000/api",
+  baseURL: " https://flipkart-clone-mgrg.onrender.com/api",
   withCredentials: true,
 });
 
